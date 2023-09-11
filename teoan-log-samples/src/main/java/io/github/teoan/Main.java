aa80e992d003ea141e2179ad6a249e58f4407946
Teoan <d277077411@gmail.com>
2023-09-11 22:40:07 +0800

    init project

---
+++ teoan-log-samples/src/main/java/io/github/teoan/Main.java
@@ -0,0 +1,11 @@
+package io.github.teoan;
+
+/**
+ * @author Teoan
+ * @since 2023/9/11 22:33
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
